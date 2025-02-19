68066559d1e5f9ae9898ceb74a94134e3e4b0c65
Galen White <galenhwhite419@gmail.com>
2025-02-18 19:25:34 -0500

    Moved some voting functions from MovieList to useVoting to keep the component clean.

--- src/components/movies/MovieList.tsx
+++ src/components/movies/MovieList.tsx
@@ -19,21 +19,18 @@ function MovieList(props: MovieListProps) {
   const { getMovieList } = useContext(MovieContext) as MovieContextType;
   // GPT recommended I null guard instead of type cast like this. I wonder what devs think is the best practice?
 
-  const { votes, subscribeToVotes, voteForMovie, unvoteForMovie } = useVoting();
+  const {
+    votes,
+    subscribeToVotes,
+    voteForMovie,
+    unvoteForMovie,
+    movieVotesReceived,
+    userVotesCast,
+    isTieBreakNeeded,
+    calculateVoteStatus,
+  } = useVoting();
 
   const [movies, setMovies] = useState<MovieType[]>([]);
-
-  const [voteStatus, setVoteStatus] = useState<{
-    leadingMovies: string[];
-    leadingVotes: number;
-    runnerUpVotes: number;
-    remainingVotes: number;
-  }>({
-    leadingMovies: [],
-    leadingVotes: 0,
-    runnerUpVotes: 0,
-    remainingVotes: 0,
-  });
 
   useEffect(() => {
     if (props.context === "group" && props.userId && props.groupId) {
@@ -51,6 +48,11 @@ function MovieList(props: MovieListProps) {
     fetchMovies();
   }, [props.userId]);
 
+  useEffect(() => {
+    if (props.votesAllowed && props.groupMembers)
+      calculateVoteStatus(props.votesAllowed, props.groupMembers.length - 1);
+  }, [votes]);
+
   const handleCheckboxChange = async (
     userId: string,
     movieId: string,
@@ -62,15 +64,15 @@ function MovieList(props: MovieListProps) {
       }
       if (e.target.checked) {
         await voteForMovie({
-          userId: userId,
-          votingUserId: user.uid,
+          memberId: userId,
+          votingMemberId: user.uid,
           movieId: movieId,
           groupId: props.groupId,
         });
       } else {
         await unvoteForMovie({
-          userId: userId,
-          votingUserId: user.uid,
+          memberId: userId,
+          votingMemberId: user.uid,
           movieId: movieId,
           groupId: props.groupId,
         });
@@ -81,132 +83,6 @@ function MovieList(props: MovieListProps) {
       // Modify this message later perhaps
     }
   };
-
-  const userVotesCast: number | undefined = votes?.votesReceived?.find(
-    (targetUser) => targetUser.votingMember === user?.uid
-  )?.votesCast;
-
-  const movieVotesReceived = (movieId: string): number => {
-    const targetMovieVotes = votes?.selectedMovies.find(
-      (targetMovie) => targetMovie.movieId === movieId
-    )?.totalVotes;
-    if (targetMovieVotes) return targetMovieVotes;
-    return 0;
-  };
-
-  // Move this to VotingContext
-  const isTieBreakNeeded = (): boolean => {
-    if (
-      props.context !== "group" ||
-      !votes?.votesReceived ||
-      !props.votesAllowed ||
-      !props.groupMembers
-    )
-      return false;
-
-    const votesReceived = votes.votesReceived;
-    const votesAllowed = props.votesAllowed;
-
-    if (voteStatus.leadingMovies.length < 2) return false;
-
-    if (props.groupMembers.length - 1 === votes.votesReceived.length) {
-      const allVotesCast = votesReceived.every(
-        (member) => member.votesCast >= votesAllowed
-      );
-      if (allVotesCast) return true;
-    }
-
-    return (
-      voteStatus.runnerUpVotes + voteStatus.remainingVotes <
-      voteStatus.leadingVotes
-    );
-  };
-
-  // Move this to VotingContext
-  const isVotingDecided = (): boolean => {
-    if (props.context === "group" && props.groupMembers && props.votesAllowed) {
-      if (voteStatus.leadingMovies.length === 0) return false;
-
-      const groupMembers = props.groupMembers;
-      const votesAllowed = props.votesAllowed;
-
-      if (groupMembers.length - 1 === votes?.votesReceived.length) {
-        const allVotesCast = votes?.votesReceived.every(
-          (member) => member.votesCast >= votesAllowed
-        );
-        if (allVotesCast && voteStatus.leadingMovies.length === 1) return true;
-      }
-
-      return (
-        voteStatus.leadingMovies.length === 1 &&
-        voteStatus.leadingVotes >
-          voteStatus.runnerUpVotes + voteStatus.remainingVotes
-      );
-    }
-    return false;
-  };
-
-  useEffect(() => {
-    // Move most of this logic to VotingContext
-    if (
-      !votes?.selectedMovies ||
-      !votes?.votesReceived ||
-      !props.votesAllowed
-    ) {
-      setVoteStatus({
-        leadingMovies: [],
-        leadingVotes: 0,
-        runnerUpVotes: 0,
-        remainingVotes: 0,
-      });
-    } else {
-      const voteCounts: Record<string, number> = {};
-      for (let movie of votes?.selectedMovies) {
-        voteCounts[movie.movieId] = movie.totalVotes || 0;
-      }
-
-      const sortedMovies = Object.entries(voteCounts).sort(
-        (a, b) => b[1] - a[1]
-      );
-
-      if (sortedMovies.length === 0) {
-        setVoteStatus({
-          leadingMovies: [],
-          leadingVotes: 0,
-          runnerUpVotes: 0,
-          remainingVotes: 0,
-        });
-        return;
-      }
-
-      const leadVotes = sortedMovies[0][1];
-      const leadingMoviesList = sortedMovies
-        .filter(([_, votes]) => votes === leadVotes)
-        .map(([movieId]) => movieId);
-
-      const highestVotes = sortedMovies[0][1];
-
-      const nextHighestVotes =
-        sortedMovies.length > leadingMoviesList.length
-          ? sortedMovies[leadingMoviesList.length][1]
-          : 0;
-
-      let totalRemainingVotes = 0;
-      for (let member of votes.votesReceived) {
-        totalRemainingVotes += props.votesAllowed - member.votesCast;
-      }
-
-      setVoteStatus({
-        leadingMovies: leadingMoviesList,
-        leadingVotes: highestVotes,
-        runnerUpVotes: nextHighestVotes,
-        remainingVotes: totalRemainingVotes,
-      });
-      if (isVotingDecided())
-        console.log("Movie selected with ID:", voteStatus.leadingMovies[0]);
-      // Create "selectMovie" method and call it here.
-    }
-  }, [votes]);
 
   return (
     <>
@@ -265,7 +141,12 @@ function MovieList(props: MovieListProps) {
             {props.context === "group" &&
             user?.uid === props.userId &&
             movieVotesReceived(movie.id) >= 1 &&
-            isTieBreakNeeded() ? (
+            props.votesAllowed &&
+            props.groupMembers &&
+            isTieBreakNeeded(
+              props.votesAllowed,
+              props.groupMembers.length - 1
+            ) ? (
               // All votes should be in.
               <>
                 <input
--- src/hooks/useVoting.ts
+++ src/hooks/useVoting.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { db } from "../utility/Firebase";
 import {
   doc,
@@ -7,13 +7,16 @@ import {
   Unsubscribe,
 } from "firebase/firestore";
 import { MemberType } from "../types/GroupType";
+import { AuthContext } from "../context/AuthContext";
 
 interface VoteParams {
-  userId: string;
-  votingUserId: string;
+  memberId: string;
+  votingMemberId: string;
   movieId: string;
   groupId: string;
 }
+
+const { user } = useContext(AuthContext);
 
 const useVoting = () => {
   const [votes, setVotes] = useState<Pick<
@@ -23,32 +26,43 @@ const useVoting = () => {
 
   const [unsubscribe, setUnsubscribe] = useState<Unsubscribe | null>(null);
 
-  const subscribeToVotes = (userId: string, groupId: string): void => {
+  const [voteStatus, setVoteStatus] = useState<{
+    leadingMovies: string[];
+    leadingVotes: number;
+    runnerUpVotes: number;
+    remainingVotes: number;
+  }>({
+    leadingMovies: [],
+    leadingVotes: 0,
+    runnerUpVotes: 0,
+    remainingVotes: 0,
+  });
+
+  const subscribeToVotes = (memberId: string, groupId: string): void => {
     if (unsubscribe) unsubscribe();
 
-    const memberRef = doc(db, `groups/${groupId}/members`, userId);
+    const memberRef = doc(db, `groups/${groupId}/members`, memberId);
     const newUnsubscribe = onSnapshot(memberRef, (snapshot) => {
       if (snapshot.exists()) {
         setVotes({
           selectedMovies: snapshot.data().selectedMovies,
           votesReceived: snapshot.data().votesReceived,
         });
-        console.log("Votes:", votes);
       } else setVotes(null);
     });
     setUnsubscribe(() => newUnsubscribe);
   };
 
   const voteForMovie = async ({
-    userId,
-    votingUserId,
+    memberId,
+    votingMemberId,
     movieId,
     groupId,
   }: VoteParams) => {
     try {
       await runTransaction(db, async (transaction) => {
         const groupRef = doc(db, "groups", groupId);
-        const memberRef = doc(db, `groups/${groupId}/members`, userId);
+        const memberRef = doc(db, `groups/${groupId}/members`, memberId);
 
         const groupSnap = await transaction.get(groupRef);
         const memberSnap = await transaction.get(memberRef);
@@ -75,22 +89,22 @@ const useVoting = () => {
           selectedMovies.push({
             movieId: movieId,
             totalVotes: 1,
-            votedBy: [votingUserId],
+            votedBy: [votingMemberId],
           });
-        } else if (!targetMovie.votedBy.includes(votingUserId)) {
+        } else if (!targetMovie.votedBy.includes(votingMemberId)) {
           targetMovie.totalVotes += 1;
-          targetMovie.votedBy.push(votingUserId);
+          targetMovie.votedBy.push(votingMemberId);
         } else {
           throw new Error("User has already voted for this movie.");
         }
 
         const userVote = votesReceived.find(
           (v: MemberType["votesReceived"][number]) =>
-            v.votingMember === votingUserId
+            v.votingMember === votingMemberId
         );
 
         if (!userVote) {
-          votesReceived.push({ votingMember: votingUserId, votesCast: 1 });
+          votesReceived.push({ votingMember: votingMemberId, votesCast: 1 });
         } else if (userVote.votesCast < votesAllowed) {
           userVote.votesCast += 1;
         } else {
@@ -111,14 +125,14 @@ const useVoting = () => {
   };
 
   const unvoteForMovie = async ({
-    userId,
-    votingUserId,
+    memberId,
+    votingMemberId,
     movieId,
     groupId,
   }: VoteParams) => {
     try {
       await runTransaction(db, async (transaction) => {
-        const memberRef = doc(db, `groups/${groupId}/members`, userId);
+        const memberRef = doc(db, `groups/${groupId}/members`, memberId);
         const memberSnap = await transaction.get(memberRef);
 
         const memberData = memberSnap.data();
@@ -138,7 +152,7 @@ const useVoting = () => {
 
         targetMovie.totalVotes -= 1;
         targetMovie.votedBy = targetMovie.votedBy.filter(
-          (targetUserId: string) => targetUserId !== votingUserId
+          (targetMemberId: string) => targetMemberId !== votingMemberId
         );
 
         if (targetMovie.totalVotes === 0) {
@@ -147,7 +161,7 @@ const useVoting = () => {
 
         const userVoteIndex = votesReceived.findIndex(
           (v: MemberType["votesReceived"][number]) =>
-            v.votingMember === votingUserId
+            v.votingMember === votingMemberId
         );
 
         if (userVoteIndex === -1) {
@@ -157,7 +171,6 @@ const useVoting = () => {
         const userVote = votesReceived[userVoteIndex];
 
         userVote.votesCast -= 1;
-
         // No need to remove userVoteIndex from votesReceived. Users will vote on other movies.
 
         transaction.set(
@@ -172,7 +185,132 @@ const useVoting = () => {
       // Edit later
     }
   };
-  return { votes, subscribeToVotes, voteForMovie, unvoteForMovie };
+
+  const movieVotesReceived = (movieId: string): number => {
+    const targetMovieVotes = votes?.selectedMovies.find(
+      (targetMovie) => targetMovie.movieId === movieId
+    )?.totalVotes;
+    if (targetMovieVotes) return targetMovieVotes;
+    return 0;
+  };
+
+  const userVotesCast: number | undefined = votes?.votesReceived?.find(
+    (targetUser) => targetUser.votingMember === user?.uid
+  )?.votesCast;
+
+  const isTieBreakNeeded = (
+    votesAllowed: number,
+    totalVoters: number
+  ): boolean => {
+    if (!votes?.votesReceived) return false;
+
+    const votesReceived = votes.votesReceived;
+
+    if (voteStatus.leadingMovies.length < 2) return false;
+
+    if (totalVoters === votes.votesReceived.length) {
+      const allVotesCast = votesReceived.every(
+        (member) => member.votesCast >= votesAllowed
+      );
+      if (allVotesCast) return true;
+    }
+
+    return (
+      voteStatus.runnerUpVotes + voteStatus.remainingVotes <
+      voteStatus.leadingVotes
+    );
+  };
+
+  const isVotingDecided = (
+    votesAllowed: number,
+    totalVoters: number
+  ): boolean => {
+    if (voteStatus.leadingMovies.length === 0) return false;
+
+    if (totalVoters === votes?.votesReceived.length) {
+      const allVotesCast = votes?.votesReceived.every(
+        (member) => member.votesCast >= votesAllowed
+      );
+      if (allVotesCast && voteStatus.leadingMovies.length === 1) return true;
+    }
+
+    return (
+      voteStatus.leadingMovies.length === 1 &&
+      voteStatus.leadingVotes >
+        voteStatus.runnerUpVotes + voteStatus.remainingVotes
+    );
+  };
+
+  const calculateVoteStatus = (votesAllowed: number, totalVoters: number) => {
+    if (!votes?.selectedMovies || !votes?.votesReceived) {
+      setVoteStatus({
+        leadingMovies: [],
+        leadingVotes: 0,
+        runnerUpVotes: 0,
+        remainingVotes: 0,
+      });
+    } else {
+      const voteCounts: Record<string, number> = {};
+      for (let movie of votes?.selectedMovies) {
+        voteCounts[movie.movieId] = movie.totalVotes || 0;
+      }
+
+      const sortedMovies = Object.entries(voteCounts).sort(
+        (a, b) => b[1] - a[1]
+      );
+
+      if (sortedMovies.length === 0) {
+        setVoteStatus({
+          leadingMovies: [],
+          leadingVotes: 0,
+          runnerUpVotes: 0,
+          remainingVotes: 0,
+        });
+        return;
+      }
+
+      const leadVotes = sortedMovies[0][1];
+      const leadingMoviesList = sortedMovies
+        .filter(([_, votes]) => votes === leadVotes)
+        .map(([movieId]) => movieId);
+
+      const highestVotes = sortedMovies[0][1];
+
+      const nextHighestVotes =
+        sortedMovies.length > leadingMoviesList.length
+          ? sortedMovies[leadingMoviesList.length][1]
+          : 0;
+
+      let totalRemainingVotes = 0;
+      for (let member of votes.votesReceived) {
+        totalRemainingVotes += votesAllowed - member.votesCast;
+      }
+
+      setVoteStatus({
+        leadingMovies: leadingMoviesList,
+        leadingVotes: highestVotes,
+        runnerUpVotes: nextHighestVotes,
+        remainingVotes: totalRemainingVotes,
+      });
+      if (isVotingDecided(votesAllowed, totalVoters))
+        console.log("Movie selected with ID:", voteStatus.leadingMovies[0]);
+      // Create "selectMovie" method and call it here.
+    }
+  };
+
+  return {
+    votes,
+    voteStatus,
+    setVoteStatus,
+    subscribeToVotes,
+    voteForMovie,
+    unvoteForMovie,
+    userVotesCast,
+    movieVotesReceived,
+    isTieBreakNeeded,
+    isVotingDecided,
+    calculateVoteStatus,
+  };
 };
 
 export default useVoting;
